08f2b3d343ab11d855205e1ca15f4a6be0893786
Can Yu <fineaisa@gmail.com>
2026-01-31 00:49:50 +0800

    docs: add Web Chat method for Telegram pairing
    - Add Web Chat option as alternative to terminal for approving Telegram pairing
    - Update all 6 language versions (EN, zh-TW, zh-CN, ja, es, id)
    - Users can now type 'openclaw pairing approve telegram <code>' in web chat

--- clawdbot/deploy.sh
+++ clawdbot/deploy.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-TEMPLATE_FILE="zeabur-template-moltbot-new-version.yaml"
+TEMPLATE_FILE="zeabur-template-clawdbot-VTZ4FX.yaml"
 PROJECT_NAME="${1:-clawdbot-$(date +%Y%m%d%H%M%S)}"
 REGION="${2:-hkg1}"
 
